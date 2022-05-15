95ae85f64967ea62dca99ecf0b7a99cf7ab27496
rawoon <rawoon35@gmail.com>
2022-05-15 15:19:20 +0900

    DmgRate(anything write)

--- src/Observer/DmgRate.java
+++ src/Observer/DmgRate.java
@@ -1,4 +1,32 @@
 package Observer;
+import Game;
 // 피해량 출력
 public class DmgRate {
+    String player;
+    int cell;
+    int enemycell;
+
+    //생성자
+    public DmgRate(String p)  {
+
+        player = p;
+    }
+
+    // 공격이 상대방에게 맞았다면
+    public void attack(enemySubM enemy) {
+        if ( enemycell > 0 ) {
+
+            enemy.enemycell  = enemy.enemycell - 2;
+            System.out.println("적 함대의 체력이 2만큼 닳았습니다");
+            System.out.println("적 함대의 남은 체력 : " + enemycell);
+
+            if (enemy.enemycell < 1) {
+                System.out.println("적 함대가 쓰러졌습니다");
+
+                if (enemy < 0) {
+                    System.out.println("Victory!");
+                }
+            }
+        }
+    }
 }
