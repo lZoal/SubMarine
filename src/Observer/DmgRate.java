a7574bca1441f5c3fe7ed1488e44bae65267a64a
rawoon <rawoon35@gmail.com>
2022-05-15 15:36:11 +0900

    DmgRate(anything write).ver2

--- src/Observer/DmgRate.java
+++ src/Observer/DmgRate.java
@@ -3,8 +3,14 @@ import Game;
 // 피해량 출력
 public class DmgRate {
     String player;
-    int cell;
+
+    // 임시 변수 생성(다른 클래스에서 호출받을 것들)
+    int mycell;
     int enemycell;
+
+    // SubM 과 enemySubM 클래스 호출
+    SubM mysub = new SubM();
+    enemySubM enemy = new enemySubM();
 
     //생성자
     public DmgRate(String p)  {
@@ -23,8 +29,26 @@ public class DmgRate {
             if (enemy.enemycell < 1) {
                 System.out.println("적 함대가 쓰러졌습니다");
 
-                if (enemy < 0) {
+                if (enemy.enemyNum < 1) {
                     System.out.println("Victory!");
+                }
+            }
+        }
+    }
+
+    // 내가 공격을 당했다면
+    public void attack(SubM mysub) {
+        if ( enemycell > 0 ) {
+
+            // 공격 당했을때의 줄어든 체력 계산 및 남은 체력 출력
+            mysub.mycell  = mysub.mycell - 1;
+            System.out.println("내 함대의 체력이 1만큼 닳았습니다");
+            System.out.println("내 함대의 남은 체력 : " + mycell);
+
+            // 내 체력이 다 닳았다면
+            if (mysub.mycell < 1) {
+                System.out.println("Lose..");
+
                 }
             }
         }
